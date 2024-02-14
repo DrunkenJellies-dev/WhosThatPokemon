c70d699ae785622bdb76dcedd60d502c6e134a4a
Callum Newman <ballumnew@gmail.com>
2024-02-14 21:09:24 +0000

    Added comments to the javascript and fixed 2 bugs
    Bug 1: The pokemon description would remain if the original mode was easy and then the user selects hard
    
    Bug 2: The name entered into the text box isn't cleared when the next pokemon is shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,10 @@ document.addEventListener("DOMContentLoaded", async function(){
 async function runGame(difficulty){
     toggleAnswerType(difficulty, document.getElementById("text-guess-container"), document.getElementById("multiple-choice-container"));
 
+    //Clears the Text box f the previous answer
+    document.getElementById("answer-box").value = "";
+
+    //Generates a random number and 
     let pokemonId = Math.floor((Math.random() * pokemonCount) + 1);
     correctPokemon = await getPokemon(pokemonId, difficulty);
 
@@ -44,20 +48,26 @@ async function runGame(difficulty){
 }
 
 async function getPokemon(pokemonId, difficulty){
+    //initializes the pokeapi url with the pokemon ID that is randomly generated 
     let pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemonId.toString();
     
+    //Receives teh information from PokeAPI and represents it as a JSON
     let res = await fetch(pokeApiUrl);
     let pokemon = await res.json();
 
+    //Gets the pokemon Name, types and image from the JSON
     let pokemonName = pokemon.name;
     let pokemonType = pokemon.types;
     let pokemonImg = pokemon["sprites"]["other"]["official-artwork"]["front_default"];
 
+    //Receives the extended pokemon information and represents it as a JSON
     res = await fetch(pokemon.species.url);
     let pokemonDesc = await res.json();
 
+    //Gets the pokemon description from the JSON
     pokemonDesc = pokemonDesc["flavor_text_entries"][11]["flavor_text"];
 
+    //Returns all information gathered as an object
     return {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc, "diff" : difficulty};
 }
 
@@ -106,18 +116,25 @@ function displayPokemon(difficulty){
                 pokemonDescription.innerHTML = correctPokemon.desc;
             }
         } else {
+            //Hides the pokemon description for Medium mode
             toggleHidden(pokemonDescription, true);
         }
     } else {
+        //Hides both the pokemon Description and types for Hard mode
         toggleHidden(typesDiv, true);
+        toggleHidden(pokemonDescription, true);
     }
 }
 
 async function populateMultipleChoice(correctPokemonName){
+    //initializes the multiple choice buttons and a pokemon names array 
     let multipleChoiceButtons = document.getElementsByClassName('multiple-choice');
     let pokemonNames = [];
 
+    //sets the correct answer in the array 
     pokemonNames[0] = correctPokemonName;
+
+    //loops through 3 of the multiple choice buttons and enters a randomly generated pokemon name into the array
     for (let i = 1; i < multipleChoiceButtons.length; i++){
         let pokemonId = Math.floor((Math.random() * pokemonCount) + 1);
         let pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemonId.toString();
@@ -128,8 +145,10 @@ async function populateMultipleChoice(correctPokemonName){
         pokemonNames[i] = pokemon.name;
     }
 
+    //shuffles the array of pokemon names so that the correct answer isn't always in the same space 
     let shuffledPokemonNames = shuffleArray(pokemonNames);
 
+    //sets the names generated in the buttons
     for (let i = 0; i < multipleChoiceButtons.length; i++){
         multipleChoiceButtons[i].innerText = shuffledPokemonNames[i].toUpperCase();
     }
@@ -139,31 +158,33 @@ function checkTextAnswer(){
     //Gets the use input and correct name of the pokemon
     let userAnswer = document.getElementById('answer-box').value.toLowerCase();
 
-    console.log(userAnswer);
-
+    //removes the silhouette from the pokemon image
     toggleSilhouette(document.getElementById('pokemon-image'), false);
 
     //Checks if the answer is correct
     differentiateAnswer(userAnswer);
 
+    //lets the Program wait for 2 seconds to let the user see the pokemon without a silhouette before starting the next game
     setTimeout(() => {runGame(correctPokemon.diff); }, 2000);
 }
 
 function checkMultipleChoiceAnswer(userAnswer){
 
+    //removes the silhouette from the pokemon image
     toggleSilhouette(document.getElementById('pokemon-image'), false);
 
     //Checks if the answer is correct
     differentiateAnswer(userAnswer);
 
+    //lets the Program wait for 2 seconds to let the user see the pokemon without a silhouette before starting the next game
     setTimeout(() => {runGame(correctPokemon.diff); }, 2000);
 }
 
 function differentiateAnswer(userAnswer){
+    //Gets the correct Answer
     let pokemonNameAnswer = correctPokemon.name;
 
-    console.log(pokemonNameAnswer);
-
+    //Checks if the correct Pokemon name is the same as the pokemon Name 
     if (userAnswer === pokemonNameAnswer){
         alert(`Correct Answer`);
         incrementCorrectAnswer();
@@ -174,35 +195,47 @@ function differentiateAnswer(userAnswer){
 }
 
 function incrementCorrectAnswer(){
+    //Gets the current score and streak
     let score = parseInt(document.getElementById('score').innerText);
     let streak = parseInt(document.getElementById('current-streak').innerText);
 
+    //Increments the score and streak by 1
     document.getElementById('score').innerText = ++score;
     document.getElementById('current-streak').innerText = ++streak;
 
+    //checks if the current streak is bigger than the highest streak and sets it 
     setHighestStreak(streak);
 }
 
 function incrementWrongAnswer(){
+    //Gets the current incorrect answer count 
     let incorrect = parseInt(document.getElementById(`incorrect`).innerText);
 
+    //increments the incorrect answer count by one
     document.getElementById(`incorrect`).innerText = ++incorrect;
+
+    //Sets the current streak back to 0
     document.getElementById('current-streak').innerText = 0;
 }
 
 function setHighestStreak(currentStreak){
+    //Gets the current highest streak 
     let highestStreak = parseInt(document.getElementById('highest-streak').innerText);
 
+    //Checks if the current streak is bigger than the highest streak and sets it if true
     if (currentStreak > highestStreak){
         document.getElementById('highest-streak').innerText = currentStreak;
     }
 }
 
 function toggleAnswerType(difficulty, textElement, multipleElement) {
+    //Sets the answer type based on the difficulty selected
     if (difficulty === 'easy') {
+        //multiple choice answering shown if it's on easy mode
         toggleHidden(textElement, true);
         toggleHidden(multipleElement, false);
     } else {
+        //text answering shown if it's on easy mode
         toggleHidden(textElement, false);
         toggleHidden(multipleElement, true);
     }
@@ -210,16 +243,20 @@ function toggleAnswerType(difficulty, textElement, multipleElement) {
 
 function toggleHidden(element, shouldBeHidden) {
     if (shouldBeHidden) {
+        //hides the element if true 
         element.classList.add("hidden");
     } else {
+        //displays the element if true
         element.classList.remove("hidden");
     }
 }
 
 function toggleSilhouette(element, shouldBeSilhouette){
     if (shouldBeSilhouette) {
+        //hides the pokemon image if true
         element.classList.add("silhouette");
     } else {
+        //displays the Pokemon image if false 
         element.classList.remove("silhouette");
     }
 }
